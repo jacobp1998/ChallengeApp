8b52ebdd2b360da028145acb2163e9967d3ce309
jacobp1998 <jakub.gajda88@gmail.com>
2023-02-12 14:57:56 +0100

    zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//zadeklarujesz zmienną z imieniem
+//zadeklarujesz zmienną z płcią ( dobierz zmienną)
+//zadeklarujesz zmieną z wiekiem
+///zweryfikujesz dane i wyświeltisz:
+///jeden zkomunikatów:
+///(1)"Kobieta poniżej  30 lat
+///(2)Ewa, lat 33
+///(3)Niepełnoletni mężczyzna
+
+string name = "Ewa";
+bool male = true;
+bool female = false;
+var age = 29;
+var age2 = 33;
+var age3=17;
+if(female==false && age<30)
+{
+    Console.WriteLine("Kobieta poniżej  30 lat");
+}
+if (name == "Ewa" && age2 == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+if (male == true && age3 < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else 
+{
+    Console.WriteLine("jestem kimś innym");
+}
+
