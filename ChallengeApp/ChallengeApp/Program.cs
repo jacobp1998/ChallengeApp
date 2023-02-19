d25fe7980cfa1e73de5c986136248b839eb52e88
jacobp1998 <jakub.gajda88@gmail.com>
2023-02-19 18:09:23 +0100

    zadanie domowe dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,32 +1,68 @@
-﻿//zadeklarujesz zmienną z imieniem
-//zadeklarujesz zmienną z płcią ( dobierz zmienną)
-//zadeklarujesz zmieną z wiekiem
-///zweryfikujesz dane i wyświeltisz:
-///jeden zkomunikatów:
-///(1)"Kobieta poniżej  30 lat
-///(2)Ewa, lat 33
-///(3)Niepełnoletni mężczyzna
+﻿int number = 1234567890;
+string numberAsString=number.ToString();
+char[] signs =numberAsString.ToArray();
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+foreach(char sign in signs)
+{
+    if (sign == '0')
+    {
+        counter0++;
+    }
+    else if (sign == '1')
+    {
+        counter1++;
+    }
+    else if (sign == '2')
+    {
+        counter2++;
+    }
+    else if (sign == '3')
+    {
+        counter3++;
+    }
+    else if (sign == '4')
+    {
+        counter4++;
+    }
+    else if (sign == '5')
+    {
+        counter5++;
+    }
+    else if (sign == '6')
+    {
+        counter6++;
+    }
+    else if (sign == '7')
+    {
+        counter7++;
+    }
+    else if (sign == '8')
+    {
+        counter8++;
+    }
+    else if (sign == '9')
+    {
+        counter9++;
+    }
+}
 
-string name = "Ewa";
-bool male = true;
-bool female = false;
-var age = 29;
-var age2 = 33;
-var age3=17;
-if(female==false && age<30)
-{
-    Console.WriteLine("Kobieta poniżej  30 lat");
-}
-else if  (name == "Ewa" && age2 == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if  (male == true && age3 < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else 
-{
-    Console.WriteLine("jestem kimś innym");
-}
-
+Console.WriteLine("LICZBA"+ number);
+Console.WriteLine("0=>" + counter0);
+Console.WriteLine("1=>" + counter1);
+Console.WriteLine("2=>" + counter2);
+Console.WriteLine("3=>" + counter3);
+Console.WriteLine("4=>" + counter4);
+Console.WriteLine("5=>" + counter5);
+Console.WriteLine("6=>" + counter6);
+Console.WriteLine("7=>" + counter7);
+Console.WriteLine("8=>" + counter8);
+Console.WriteLine("9=>" + counter9);
