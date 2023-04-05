5df8feecc9a79de2370a2149913812848a25ba1b
jacobp1998 <jakub.gajda88@gmail.com>
2023-04-05 14:48:06 +0200

    zadanie 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();          // lista
+
+
+        public Employee(string name, string surname, int age) //konstruktor
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }  //propercja 1 
+        public string Surname { get; private set; }  //propercja 2 
+        public int Age { get; private set; }  // propercja 3 
+
+
+
+        public void AddScore(int number)  //metoda  1
+        {
+            this.score.Add(number);
+        }
+
+
+        public int Result   //metoda  2 
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,98 +1,55 @@
-﻿// klasy, to struktury, które są przygotowane aby obsługiwać powtarzające się modele danych
-//np. użytkowanik danego systemu (ma swój login, hasło)
-//np. raport 
-//książka w magazynie, zamówienie w sklepie
-//User user1=new User();  var user1 = new User();
-//User user2= new User(); var user2 = new User();
-//User user3= new User(); var user3 = new User();
-//User user4= new User(); var user4 = new User();
-
-//user1.login = "Jakub";
-//user1.password
-//class User
-//{
-   // public string login;
-   // public string password;
-   // private string name;
-//}
-
-// public i private - modyfikatory dostępu.
-//private - coś czego nie można zmienić np. imię; jeśli nie ma napisane private to i tak jest to domyślnie private.
+﻿using ChallengeApp;
 
 
+Employee employee1 = new Employee("Jakub", "Gajda",25);
+Employee employee2 = new Employee("Marcin","Wesołek",29);
+Employee employee3 = new Employee("Kamil","Małek",21);
+
+employee1.AddScore(10);
+employee1.AddScore(10);
+employee1.AddScore(10);
+employee1.AddScore(10);
+employee1.AddScore(10);
+
+employee2.AddScore(9);
+employee2.AddScore(9);
+employee2.AddScore(9);
+employee2.AddScore(9);
+employee2.AddScore(9);
+
+employee3.AddScore(1);
+employee3.AddScore(1);
+employee3.AddScore(1);
+employee3.AddScore(1);
+employee3.AddScore(1);
+
+//var result1 = employee1.Result;
+//Console.WriteLine(result1); 
+//var result2 = employee2.Result;
+//Console.WriteLine(result2); 
+//var result3 = employee3.Result; 
+//Console.WriteLine(result3); 
 
 
-//KONSTRUKTORY: dodatkowe miejsce, które jest wywoływane w momencie, gdy tworzony jest dany obiekt.
 
-
-
-
-
-    //02.04.2023
-
-    User user1= new User("Adam", "456456456");
-    User user2= new User("Monika", "456456456");  
-    User user3= new User("Zuzia", "456456456");  
-    User user4= new User("Damian", "456456456");
-
-
-//user1.Login = "Marek";  //ctrl+spacja (podpowiedź) pojawia się Login 
-var name = user1.Login;
-
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
-var name = User.GameName;
-var pi = Math.PI;
-class User                     //klasa
+List<Employee> employees = new List<Employee>()
 {
+employee1, employee2, employee3
+};
 
-    public static string GameName = "Diablo";            //obiekt statyczny, można bezpośrednio działać na nim , bez tworzenia obiektu User typu user1.
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
 
-    private List <int> score = new List<int>();                //zmienna, którą lokalnie przechowywujemy
-   
-    public User(string login, string password)      //konstruktor
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
     {
-        this.Login = login;
-        this.Password = password;
-        
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
     }
-
-    
-
-    public string Login { get; private set; }             //propercja (pola) 1
-
-
-    public string Password { get; private set; }  //propercja(pole) 2 
-
-
-
-    public int Result                               //METODA 
-    {
-        get
-        {
-            return this.score.Sum();
-        }
-    }
-
-
-    public void AddScore(int number )               //METODA
-    {
-        this.score.Add(number);
-    }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
+Console.WriteLine("Pracownik z najwyższą liczbą ocen : ");
+Console.WriteLine(employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " " + "lat" + " " + employeeWithMaxResult.Age );
+//dfghjkm,lqwerftgyhjkwedfghjyjc
+//wetsrydtf
+    
